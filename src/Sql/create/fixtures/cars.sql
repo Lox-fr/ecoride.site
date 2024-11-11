e1f0c806d7a2f8a838643c1d29f5ca4346ddc613
Lox <loic.petit@gmx.com>
2024-11-11 18:28:17 +0100

    feat: Update and increase the number of SQL fixtures for cars, preferences, roles, users, and userRoles

--- src/Sql/create/fixtures/cars.sql
+++ src/Sql/create/fixtures/cars.sql
@@ -1,5 +1,34 @@
-INSERT INTO car (license_plate, date_of_first_registration, model, user_id, brand_id, engine_type_id, color_id)
+-- Admin - id 1
+INSERT INTO car (license_plate, date_of_first_registration, model, user_id, brand_id, engine_type_id, color_id) 
 VALUES
-    ('AB-123-CD', '2020-05-10', '3008', 3, 19, 3, 9),
-    ('EF-456-GH', '2019-07-23', 'Captur', 5, 21, 4, 6),
-    ('IJ-789-KL', '2021-02-15', 'Polo', 5, 28, 2, 4);
+    ('AB-123-CD', '2020-05-10', '3008', 1, 19, 3, 9),
+    ('EF-456-GH', '2022-03-15', 'Q5', 1, 1, 4, 6);
+
+-- Employees - id 2 and 3
+INSERT INTO car (license_plate, date_of_first_registration, model, user_id, brand_id, engine_type_id, color_id) 
+VALUES
+    ('IJ-789-KL', '2021-02-15', 'Polo', 2, 28, 2, 4),
+    ('KL-012-MN', '2021-05-22', 'Clio', 2, 21, 1, 7),
+    ('OP-345-QR', '2020-07-10', 'Captur', 3, 21, 4, 6);
+
+-- Drivers - id 4 to 15
+INSERT INTO car (license_plate, date_of_first_registration, model, user_id, brand_id, engine_type_id, color_id) 
+VALUES
+    ('ST-678-UV', '2019-09-05', 'Civic', 4, 8, 1, 4),
+    ('EF-234-GH', '2021-09-11', 'Focus', 4, 7, 1, 5),
+    ('WX-901-YZ', '2020-01-11', 'CX-5', 5, 14, 2, 6),
+    ('IJ-567-KL', '2021-12-05', 'Golf', 5, 7, 2, 9),
+    ('AB-234-CD', '2021-03-18', 'Mini Countryman', 6, 16, 4, 3),
+    ('CD-567-EF', '2022-07-23', 'Fiesta', 7, 7, 1, 5),
+    ('GH-890-HI', '2023-04-10', 'Tucson', 8, 9, 2, 5),
+    ('OP-234-QR', '2021-05-12', 'Mokka', 8, 18, 3, 5),
+    ('JK-123-LM', '2021-11-20', '3008', 9, 19, 3, 4),
+    ('NO-456-PQ', '2020-08-15', 'Citroën C3', 10, 21, 1, 9),
+    ('RS-789-UV', '2022-03-05', 'Zoé', 11, 26, 4, 6),
+    ('UV-678-WX', '2020-09-01', '2008', 11, 19, 3, 9),
+    ('TU-012-VW', '2021-06-07', 'Astra', 12, 18, 3, 4),
+    ('VW-345-XY', '2021-10-02', 'Duster', 13, 4, 2, 6),
+    ('YZ-890-ZA', '2023-07-09', 'Kia Sportage', 13, 21, 4, 4),
+    ('YZ-678-ZA', '2022-05-15', 'Corsa', 14, 18, 3, 5),
+    ('AB-901-CD', '2023-02-22', 'Taycan', 15, 26, 4, 9),
+    ('BC-789-DE', '2022-03-15', 'Zoé', 15, 26, 4, 5);
