fc8d9a8456c88e35676573877edc8927e277f94f
Lox <loic.petit@gmx.com>
2024-12-02 23:14:36 +0100

    feat: Add RoleManager service to map user roles to descriptions
    - Implemented RoleManager service with role-to-description mapping.
    - Prioritizes roles in the order of ROLE_ADMIN > ROLE_EMPLOYEE > ROLE_DRIVER > ROLE_PASSENGER.
    - Returns 'Utilisateur' as a default description for unauthenticated users or unrecognized roles.

--- src/Controller/UserProfileController.php
+++ src/Controller/UserProfileController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\RoleManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,10 +12,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class UserProfileController extends AbstractController
 {
     #[Route('/profil', name: 'app_user_profile')]
-    public function index(): Response
+    public function index(RoleManager $roleManager): Response
     {
         return $this->render('user_profile/index.html.twig', [
             'controller_name' => 'UserProfileController',
+            'roleDescription' => $roleManager->getRoleDescription()
         ]);
     }
 }
