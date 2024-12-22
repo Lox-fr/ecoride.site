e06049c1ff26eb7cac40491dc0b359849466eb22
Lox <loic.petit@gmx.com>
2024-12-22 11:58:09 +0100

    refactor: Rename controller
    - Renamed "userProfileMenuButtons_controller.ts" to "profileMenu_controller.ts".
    - Updated corresponding references in HTML and Stimulus bootstrap configuration.

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -2,7 +2,7 @@ import { startStimulusApp } from "@symfony/stimulus-bridge";
 import FlatpickrController from "./controllers/flatpickr_controller.ts";
 import FileInputController from "./controllers/fileInput_controller.ts";
 import PagePreloaderController from "./controllers/pagePreloader_controller.ts";
-import ToggleProfileMenuButtonsController from "./controllers/toggleProfileMenuButtons_controller.ts";
+import profileMenuController from "./controllers/profileMenu_controller.ts";
 import TooltipsController from "./controllers/tooltips_controller.ts";
 
 const app = startStimulusApp();
@@ -10,5 +10,5 @@ const app = startStimulusApp();
 app.register("flatpickr", FlatpickrController);
 app.register("fileInput", FileInputController);
 app.register("pagePreloader", PagePreloaderController);
-app.register("toggleProfileMenuButtons", ToggleProfileMenuButtonsController);
+app.register("profileMenu", profileMenuController);
 app.register("tooltips", TooltipsController);
